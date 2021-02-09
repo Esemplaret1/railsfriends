e831089c50865c93eef0484435b37d6e73f87ea8
= <esemplaret1@gmail.com>
2021-02-09 03:42:02 -0500

    changing HTML on frontpage and about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
 
   def about
-  	@about_author = "Tom over here is learning Ruby on Rails!  He's very impressed with how CRUD functions are handled and is excited to dive deeper."
+  	@about_author = "Tom over here is learning Ruby on Rails.  He's very impressed with how CRUD functions are handled and is excited to dive deeper."
   end
 
 end
